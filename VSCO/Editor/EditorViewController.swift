ae037f12761cb7acbbf51389c6e3705c04fd5cb3
amazingtima <timur.tansikbaev@gmail.com>
2023-02-18 15:07:43 +0600

    Initial Commit

---
+++ VSCO/Editor/EditorViewController.swift
@@ -0,0 +1,45 @@
+//
+//  EditorViewController.swift
+//  VSCO
+//
+//  Created by Тимур on 18.02.2023.
+//
+
+import UIKit
+
+class EditorViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var filterView: UIView!
+    public var picture: UIImage?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.imageView.image = picture
+        filterView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+    }
+  
+    @IBAction func action1(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.blue
+    }
+    
+    @IBAction func action2(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.yellow
+    }
+    
+    @IBAction func action3(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.purple
+    }
+    
+    @IBAction func action4(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.orange
+    }
+    
+    @IBAction func action5(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.grey
+    }
+    
+    
+    
+}
